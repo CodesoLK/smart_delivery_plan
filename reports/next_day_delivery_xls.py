dec06a86272678a2bd9928dbd3ef561def712e3e
Sachitha Perera <dev.sachitha@gmail.com>
2021-03-05 15:15:08 +0530

    [Updated]Next Day delivery plan Empty report

--- reports/next_day_delivery_xls.py
+++ reports/next_day_delivery_xls.py
@@ -4,10 +4,8 @@ from flectra import fields, api, models, _
 class NextDayDeliveryReportXls(models.AbstractModel):
     _name = 'report.smart_delivery_plan.report_next_day_delivery_xls'
     _inherit = 'report.report_xlsx.abstract'
-    def generate_xlsx_report(self, workbook, data, partners):
-        for obj in partners:
-            report_name = obj.name
-            # One sheet by partner
-            sheet = workbook.add_worksheet(report_name[:31])
-            bold = workbook.add_format({'bold': True})
-            sheet.write(0, 0, obj.name, bold)
+    def generate_xlsx_report(self, workbook, data, lines):
+        # format1 = woorkbook.add_format({'font_size': 14, 'align': 'vcenter', 'bold': True})
+        # format2 = woorkbook.add_format({'font_szie': 18, 'align': 'vcenter'})
+        sheet = workbook.add_worksheet('Nexy Day Delivery Plan')
+        # sheet.write(1,1, 'Next Day Delivery Plan', bold)
